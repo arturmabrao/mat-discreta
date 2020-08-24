c45f88c5e5f5e72636eb8028462fface38e272b7
Artur Abrão <arturmabrao@gmail.com>
2020-08-24 15:39:44 -0300

    Conter

--- Primeiro/conjuntos.py
+++ Primeiro/conjuntos.py
@@ -30,17 +30,45 @@ def unir(conj1, conj2):
     uniao = retirarepeticaoeordena(uniao)
     return uniao
 
+def seestacontido(conj1, conj2):
+    # Se contem conj1 no conj2
+    conferencia = geraintersecao(conj1, conj2)
+    if len(conferencia) == len(conj1):
+        return True
+    else:
+        return False
 
-c1 = [441,1,1,1,1,6,2,3,4,0]
-c2 = [31,1,2,40,1,10,39,104]
+def contemelemento(e,conj1):
+    for i in range(0, len(conj1)):
+        if conj1[i] == e:
+            return True
+    return False
 
+
+c1 = [0,4,2]
+c2 = [1,0,4,2]
+
+e = 1
+
+# Elimina repetiçao e ordena
 c1 = retirarepeticaoeordena(c1)
 c2 = retirarepeticaoeordena(c2)
 
+# Identifica interseção
 intersecao = geraintersecao(c1,c2)
+
+# Indentifica conjunto diferença
 diferenca_1p2 = geradiferenca(c1,c2)
 diferenca_2p1 = geradiferenca(c2,c1)
+
+# Reliza união dos conjuntos
 uniao = unir(c1,c2)
+
+# Verifica se um contém o outro
+contem_c1emc2 = seestacontido(c1,c2)
+contem_c2emc1 = seestacontido(c2,c1)
+
+
 print(f"Conjunto A sem repetições e ordenado: {c1}")
 print(f"Conjunto B  sem repetições e ordenado: {c2}")
 print(f"A interseção dos conjuntos é: {intersecao}")
@@ -48,3 +76,28 @@ print(f"A diferença de A para B: {diferenca_1p2}")
 print(f"A diferença de B para A: {diferenca_2p1}")
 print(f"União de A com B: {uniao}")
 
+# Retorna sobre os conjuntos serem iguais ou um conter o outro
+if contem_c2emc1 == True and contem_c1emc2 == True:
+    print("Os conjuntos A e B são iguais")
+else:
+    if contem_c2emc1 == True:
+        print(f"Conjunto B está contido no conjunto A")
+    elif contem_c1emc2 == True:
+        print(f"Conjunto A está contido no conjunto B")
+    else:
+        print(f"Os conjuntos não contêm um ao outro")
+
+# Se há o elemento nos conjuntos
+c1ce = contemelemento(e,c1)
+c2ce = contemelemento(e,c2)
+
+# Retorna sobre os conjuntos conterem o elemento e
+if c1ce == True and c2ce == True:
+    print(f"Ambos conjuntos conjuntos têm o elemento {e}")
+else:
+    if c1ce == True:
+        print(f"{e} está presente no conjunto A")
+    elif c2ce == True:
+        print(f"{e} está presente no conjunto B")
+    else:
+        print(f"{e} não está presente em nenhum dos conjuntos")
