ad195ad8fac38da739b01507c55bdf030334e772
Artur Abrão <arturmabrao@gmail.com>
2020-08-26 22:32:38 -0300

    Ini

--- Primeiro/main.py
+++ Primeiro/main.py
@@ -3,10 +3,12 @@ from conjunto import Conjunto
 # nome = input(f'Digitar novo elemento? [S/N] ')
 
 
-c1 = Conjunto('A')
-c2 = Conjunto('B')
+c1: object = Conjunto('A', [1, 2, 3])
+c2: object = Conjunto('B', [3, 4, 5, 6])
 # c1 = c1.recebeConj()
-# c2 = c2.recebeConj().
-c1n = c1.getNome()
+# c2 = c2.recebeConj()
+# c1.setElementos(conj)
 # print(f'{c1}\n{c2}')
-print(f'{c1n}')
+# print(f'{c1n}')
+print(f'{c1.nome}, {c1.elementos}')
+print(f'{c2.nome}, {c2.elementos}')
